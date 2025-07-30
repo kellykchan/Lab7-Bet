cf3f86144f05cdb80826a680275d1cb12d4fad54
Kelly Chan <152578750+kellykchan@users.noreply.github.com>
2025-07-30 14:14:39 +0000

    Completed

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://nnhyjhfjoguikjogankp.supabase.co'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uaHlqaGZqb2d1aWtqb2dhbmtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4MTY0NTIsImV4cCI6MjA2OTM5MjQ1Mn0.PVqX6Pa1m9dL1lhEVuJdvadSxBvD9MmpzCLepYYn_ek'
+const URL = import.meta.env.VITE_SUPABASE_URL
+const API_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(URL, API_KEY)
